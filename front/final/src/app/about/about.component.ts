5ef9419fef5b73cbeab40781b29152b11568be64
Aurélien Loyer <aur.loy@gmail.com>
2018-11-11 01:04:02 -0500

    feat: display contributors 🏋️‍♂️  and links

--- front/final/src/app/about/about.component.ts
+++ front/final/src/app/about/about.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  contributors: any[] = [
+    {
+      fullname: 'Aurelien Loyer',
+      twitter: '@AurelienLoyer',
+      abstract: 'Consultant | Formateur Web - Javascript / @Angular / @Nodejs / @VueJs | 🇨🇦 @ZenikaMontreal ex 🥖 @ZenikaLille | 🤖 Domotic Fan |🐧Penguin lover',
+      image: 'https://bulma.io/images/placeholders/96x96.png',
+    },
+    {
+      fullname: 'Emmanuel Demey',
+      twitter: '@EmmanuelDemey',
+      abstract: 'CTO at @ZenikaLille - Google Developer Expert @GoogleDevExpert - Web Specialist - Speaker and Trainer, Organizer of #DevFestLille - @GDGLille - Lille',
+      image: 'https://bulma.io/images/placeholders/96x96.png',
+    },
+    {
+      fullname: 'Logan HAUSPIE',
+      twitter: '@LHauspie',
+      abstract: 'Dev back @ZenikaLille. 🖥️⌨️🖱️ Born to Code, Tech aficionado et Speaker à mes heures perdues.',
+      image: 'https://bulma.io/images/placeholders/96x96.png',
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
