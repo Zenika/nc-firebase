f111539ba0c53980cb73d021574ada87fb497ba3
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-02-04 15:02:40 +0100

    update angular project

---
+++ corrections/step1/angular/src/app/models/account.model.ts
@@ -0,0 +1,4 @@
+export interface Account {
+  handle: string;
+  id: string;
+}
--- corrections/step2/angular/src/app/board/stats/stats.component.ts
+++ corrections/step2/angular/src/app/board/stats/stats.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { Twitter } from "../../models/twitter.model";
+import { Twitter } from "src/app/models/twitter.model";
 
 @Component({
   selector: "z-stats",
